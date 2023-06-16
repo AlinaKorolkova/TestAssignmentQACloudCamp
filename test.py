add5c6b88cc12a046f1fc0701947f99674230376
antirrhinumm <lina.korolkovvva@bk.ru>
2023-06-16 21:02:10 +0300

    Test

---
+++ test.py
@@ -0,0 +1,27 @@
+import requests
+
+
+def test_get_posts():
+    response = requests.get("https://jsonplaceholder.typicode.com/posts")
+    assert response.status_code == 200
+    assert isinstance(response.json(), list)
+
+
+def test_create_post():
+    data = {
+        "title": "Test",
+        "body": "Test body",
+        "userId": 1
+    }
+    response = requests.post("https://jsonplaceholder.typicode.com/posts", json=data)
+    assert response.status_code == 201
+    assert response.json()["title"] == "Test"
+
+
+def test_delete_post():
+    response = requests.delete("https://jsonplaceholder.typicode.com/posts/1")
+    assert response.status_code == 200
+    assert response.json() == {}
+
+if __name__ == "__name__":
+    test_create_post()
